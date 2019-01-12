c492242a0cc3840e569cd1eddb68e9d9b9314b8e
tknr <geishagirlskoume@gmail.com>
2019-01-12 21:03:08 +0900

    init

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+
+echo "this command must be executed in root user."
+
+wget https://dl.eff.org/certbot-auto -O certbot-auto
+chmod a+x certbot-auto
+cp certbot-auto /usr/local/bin/
+
+certbot-auto
