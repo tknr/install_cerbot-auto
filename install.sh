2e0af91e63738fdc6a13a99e1064f40529ac458d
tknr <geishagirlskoume@gmail.com>
2019-01-12 21:10:40 +0900

    2019年  1月 12日 土曜日 21:10:40 JST

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ echo "this command must be executed in root user."
 wget https://dl.eff.org/certbot-auto -O certbot-auto
 chmod a+x certbot-auto
 cp certbot-auto /usr/local/bin/
+
 cp letsencrypt /etc/cron.d/
+chown root:root /etc/cron.d/letsencrypt
+chmod 644 /etc/cron.d/letsencrypt
 
 certbot-auto
